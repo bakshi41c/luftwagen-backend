21bcbdcbef09fb6de494f012e22e2bca4dc9808e
Shubham Bakshi <shubhambakshi@shubhams-mbp.home>
2015-12-01 00:22:28 +0000

    Added weather fetching
    - Python file to fetch weather
    - Refactored traffic.py

--- utils/traffic.py
+++ utils/traffic.py
@@ -1,24 +1,24 @@
 import math
 import urllib2
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as xml
 
 
-#https://developer.here.com/rest-apis/documentation/traffic/topics_v6.1/example-flow.html
+# https://developer.here.com/rest-apis/documentation/traffic/topics_v6.1/example-flow.html
 app_id = "tip09eTk3MPc1vqGNztA"
 app_code = "Z8z1Sd_gX-4MvRacAgihqw"
 
-def getTrafficData(coordX, coordY):
+
+def get_traffic_data(coordX, coordY):
     z = 19
-    (x_tile, y_tile) = calculateTile(coordX, coordY, 19)
-    url = formURL(int(x_tile), int(y_tile), z)
+    (x_tile, y_tile) = calculate_tile(coordX, coordY, 19)
+    url = form_url(int(x_tile), int(y_tile), z)
     print url
     req = urllib2.Request(url)
     response = urllib2.urlopen(req)
     traffic_xml = response.read()
-    #print traffic_xml
 
     # Getting the root of the XML data
-    root = ET.fromstring(traffic_xml)
+    root = xml.fromstring(traffic_xml)
 
     rw = root[0][0]
     cf = rw[0][0][1]
@@ -26,11 +26,10 @@ def getTrafficData(coordX, coordY):
     jf = float(cf.get('JF'))
 
     print rw
-    print jf.__str__()
+    return str(jf)
 
 
-
-def calculateTile(lat=0.0, lon=0.0, z=1):
+def calculate_tile(lat=0.0, lon=0.0, z=1):
     lat_rad = lat * math.pi / 180.0
     n = 2 ** z
     x_tile = n * ((lon + 180.0) / 360.0)
@@ -39,13 +38,13 @@ def calculateTile(lat=0.0, lon=0.0, z=1):
     return x_tile, y_tile
 
 
-def formURL(x,y,z):
+def form_url(x, y, z):
     url = "http://traffic.cit.api.here.com/traffic/6.1/flow/xml/" \
-      + str(z) + "/" + str(int(x)) + "/" + str(int(y)) + "?app_id=" + app_id + "&app_code=" + app_code
+          + str(z) + "/" + str(int(x)) + "/" + str(int(y)) + "?app_id=" + app_id + "&app_code=" + app_code
 
     return url
 
-
-lat = 51.5263248  # float(raw_input("lat"))
-lon = -0.1355824  # float(raw_input("lon"))
-getTrafficData(lat, lon)
+# How to use
+lat = 51.5263248
+lon = -0.1355824
+print get_traffic_data(lat, lon)
---
+++ utils/weather.py
@@ -0,0 +1,127 @@
+import urllib2
+import xml.etree.ElementTree as xml
+import os.path as systemfile
+import math
+import time
+
+metoffice_api_key = "6b57d1a1-0fc0-40dc-ae66-950b3ec03c4f"
+
+site_list_url = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/sitelist?res=daily&key=" + metoffice_api_key
+weather_url = "http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/xml/%s?res=3hourly&key=" + metoffice_api_key
+
+weather_site_filename = 'weather_sites.xml'
+
+cache_expiry = 15 * 60  # 15 minutes
+
+# Format of weather_cache => {id : (xml, expiry)}
+weather_cache = {}
+
+
+def get_nearest_weather_site(sites, lat=0.0, lon=0.0):
+    site_distances = {}
+
+    for site in sites:
+        id = int(site.get('id'))
+        site_lat = float(site.get('latitude'))
+        site_lon = float(site.get('longitude'))
+
+        distance = haversine(site_lon, site_lat, lon, lat)
+        site_distances[id] = distance
+
+    return min(site_distances.items(), key=lambda x: x[1])[0]  # finding the least value and then returning the key
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    Found from stackoverflow
+    """
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(math.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
+    c = 2 * math.asin(math.sqrt(a))
+    r = 6371  # Radius of earth in kilometers. Use 3956 for miles
+    return c * r
+
+
+def get_weather_sites_online():
+    req = urllib2.Request(site_list_url)
+    response = urllib2.urlopen(req)
+    site_list_xml = response.read()
+
+    root = xml.fromstring(site_list_xml)
+
+    f = open(weather_site_filename, 'w')
+    f.write(xml.tostring(root))
+    f.close()
+
+
+def get_weather_sites():
+    if not systemfile.exists(weather_site_filename):  # if a local copy of weather sites not available then download
+        get_weather_sites_online()
+
+    with open(weather_site_filename, "r") as sites:
+        sites_xml = sites.read()
+
+    return xml.fromstring(sites_xml)
+
+
+def cache_weather(site_id, weather_data):
+    weather_cache[site_id] = (weather_data, round(time.time()) + cache_expiry)
+
+
+def get_weather(lat, lon):
+    sites = get_weather_sites()
+    site_id = get_nearest_weather_site(sites, lat, lon)
+
+    weather_data = None
+
+    # if the data is in cache and it hasn't expired, then show the cached data otherwise fetch from internet
+    if (site_id in weather_cache) and (weather_cache[site_id][1] > time.time()):
+        print "Cached weather data"
+        weather_data = xml.fromstring(weather_cache[site_id][0])
+    else:
+        print "New weather data"
+        req = urllib2.Request(weather_url % site_id)
+        response = urllib2.urlopen(req)
+        weather_xml = xml.fromstring(response.read())
+        weather_data = weather_xml[1][0][0][-1]
+        cache_weather(site_id, xml.tostring(weather_data))  # cache the new data for future
+
+    if weather_data is None:
+        return None  # something went terribly wrong
+
+    # all the data from met office
+    temp = weather_data.get('T')
+    feels_like = weather_data.get('F')
+
+    wind_direction = weather_data.get('D')
+    wind_speed = weather_data.get('S')
+    wind_gust = weather_data.get('G')
+
+    relative_humidity = weather_data.get('H')
+    precipitation_prob = weather_data.get('Pp')
+
+    visibility = weather_data.get('V')
+    uv = weather_data.get('U')
+    weather_type = weather_data.get('W')
+
+    # NOTE: Currently only returning temperature and wind speed, but feel free to take more
+    return temp, wind_speed
+
+
+# SAMPLE usage, Delete before using it as library
+
+temp, wind_speed = get_weather(51.0123, 0.3)  # This data will be new
+print temp, wind_speed
+
+time.sleep(1)
+
+temp, wind_speed = get_weather(51.0123, 0.3)  # This data will be cached
+print temp, wind_speed
